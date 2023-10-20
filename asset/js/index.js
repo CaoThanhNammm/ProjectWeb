4a6ecf448cda42062f3f830c9c358d35ef8758de
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2023-10-20 22:08:10 +0700

    cập nhập ảnh giới thiệu, slider, thêm 1 số hiệu ứng cho product, làm banner cho sản phẩm

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,27 +1,22 @@
 
 /*
 Create: Cao Thành Named
-Date: 19/10/2023
-Note: thay đổi hình ảnh khi đưa chuột vào sản phẩm
+Date: 20/10/2023
+Note: hiện ra 2 nút thêm vào giở hàng và xem chi tiết sản phẩm
 */
 
 function hoverChangeProductImg(){
     var elementProduct = document.querySelectorAll(".product");
     
-    elementProduct.forEach(function(value, index){
-        var elementProductImg = value.querySelector(".product_img img");
+    elementProduct.forEach(function(value){
         var elementProductBtn = value.querySelectorAll(".product_btn a");
-        
+
         value.onmouseover = function(){
-            elementProductImg.setAttribute("src", "asset\\image\\jpg\\content_introduce.jpg");
-            value.style.border = "1px solid var(--gray-second)";
             elementProductBtn[0].classList.replace("product_btn-add_card_none", "product_btn-add_card");
             elementProductBtn[1].classList.replace("product_btn--product_detail_none", "product_btn--product_detail");
         }
 
         value.onmouseout = function(){
-            elementProductImg.setAttribute("src", "asset\\image\\jpg\\product_img.jpg");
-            value.style.border = "none";
             elementProductBtn[0].classList.replace("product_btn-add_card", "product_btn-add_card_none");
             elementProductBtn[1].classList.replace("product_btn--product_detail", "product_btn--product_detail_none");
         }
@@ -42,4 +37,41 @@ function addWishList(){
         }
     });
 
+}
+
+var active = 0
+var elementImgItem = document.querySelectorAll(".img_item");
+var elementImgList = document.querySelector(".img_list")
+var length = elementImgItem.length;
+var elementLeft = document.querySelector(".left");
+var elementRight = document.querySelector(".right");
+
+function moveLeft(){
+    elementLeft.onclick = function(){
+        if(active === 0) {
+            active = length;
+        }
+        active--;
+        reloadSlider();
+    }
+}
+function moveRight(){
+    elementRight.onclick = function(){
+        if(active === length - 1) {
+            active = -1;
+        }
+        active++;
+        reloadSlider();
+    }
+}
+
+function autoSlider(){
+    setInterval(function(){
+        elementRight.click();
+    }, 5000);
+}
+
+function reloadSlider(){
+    var checkLeft = elementImgItem[active].offsetLeft;
+    elementImgList.style.left = -checkLeft + "px";
 }
