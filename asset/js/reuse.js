bfcfd90fce551a877a15024e0f4fca5c5372441d
Trần Minh Quân <21130494@st.hcmuaf.edu.vn>
2023-10-25 15:20:13 +0700

    Hoàn thiện thêm phần detail (Chưa có phần đánh giá)

--- asset/js/reuse.js
+++ asset/js/reuse.js
@@ -1,12 +1,13 @@
 function _linkHeaderFooterCSS() {
     head = document.querySelector("head");
-    head.innerHTML += `
+    head.innerHTML = `
     <link rel="stylesheet" href="/asset/css/index.css">
     <link rel="stylesheet" href="/asset/css/header.css">
     <link rel="stylesheet" href="/asset/css/footer.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     `
+    + head.innerHTML;
 }
 
 /*
