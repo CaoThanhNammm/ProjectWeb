456bda7fb3d2774725e4a950a5b756028bfdd94e
21130449-NguyenKhaiNam <21130449@st.hcmuaf.edu.vn>
2023-11-09 13:46:15 +0700

    Thêm các phần cho admin

--- asset/js/detail.js
+++ asset/js/detail.js
@@ -1,32 +1,15 @@
 // JavaScript cho phần hình ảnh
 function _imageSlider() {
-  var images = document.querySelectorAll(".slider_image");
-  var mainImage = document.querySelector(".slider_main img");
+    var images = document.querySelectorAll('.slider_image');
+    var mainImage = document.querySelector('.slider_main img');
 
-  images.forEach((image, index) => {
-    image.addEventListener("click", () => {
-      images.forEach((img) => img.classList.remove("active"));
-      image.classList.add("active");
-      mainImage.src = image.src;
+    images.forEach((image, index) => {
+        image.addEventListener('click', () => {
+            images.forEach((img) => img.classList.remove('active'));
+            image.classList.add('active');
+            mainImage.src = image.src;
+        });
     });
-  });
-}
-function _refreshPageItem() {
-  elementNewPageItem.forEach(function (value) {
-    value.classList.remove("padding_down");
-    value.classList.remove("choose_page_item");
-  });
 }
 
-var elementNewPageItem = document.querySelectorAll(".new_page_item");
-function _changePage() {
-  elementNewPageItem.forEach(function (value) {
-    value.onclick = function () {
-      _refreshPageItem();
-      value.classList.add("padding_down");
-      value.classList.add("choose_page_item");
-    };
-  });
-}
-
-_imageSlider();
+_imageSlider()
