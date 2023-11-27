941232f60faf566e737675b47025a4746c68b84e
21130449-NguyenKhaiNam <21130449@st.hcmuaf.edu.vn>
2023-11-27 21:20:30 +0700

    Fix some error

--- src/main/webapp/js/access.js
+++
@@ -1,9 +0,0 @@
-/**
- * Create: Nguyễn Khải Nam
- * Date: 16/11/2023
- * Note: Bắt sự kiện của đăng nhập, đăng ký, quên mật khẩu
- */
-
-function vertifyEmail() {
-    prompt('Vui lòng nhập mã được gửi về gmail', 'Đoạn mã gồm 6 số')
-}
