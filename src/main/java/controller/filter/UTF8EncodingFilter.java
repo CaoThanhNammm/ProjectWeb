c36f29a4962752b85dd2edcc44af8943c4125173
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2023-12-03 12:17:37 +0700

    sua filter

--- src/main/java/controller/filter/UTF8EncodingFilter.java
+++ src/main/java/controller/filter/UTF8EncodingFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter("/")
+@WebFilter("/*")
 public class UTF8EncodingFilter implements Filter {
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
